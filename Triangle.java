5a037f3d61235141fae71957aac7a234c94f0424
pickle-9712 <gradennolan64@gmail.com>
2026-08-20 08:47:31 -0700

    fix

---
+++ Triangle.java
@@ -0,0 +1,8 @@
+public class Triangle {
+    public static void main(String[] args){
+        int base = 7;
+        int height = 11;
+        double area = (base*height)/2;
+        System.out.println("The area of the triangle is "+area+".");
+    }
+}
